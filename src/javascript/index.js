0aefd13551db4232de149fc7d18982f196dcbfda
SettingDust <settingdust@gmail.com>
2019-01-23 19:25:41 +0800

    Add header handler

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -8,35 +8,5 @@ const handlerList = [
 Promise.all(handlerList).then(async (e) => {
     for (const fn of e)
         await fn.default();
-
     router();
-
-    $(() => {
-        bpLogger.debug(bpVars);
-        const $main = $('main');
-        $main.on('scroll', () => {
-            bpThrottle(() => {
-                if ($main.scrollTop() > 0) {
-                    $('#header').addClass('on');
-                } else {
-                    $('#header').removeClass('on');
-                }
-            }, 400);
-        });
-
-        const $avatar = $('#avatar');
-        $avatar.children().css('background-image', `url(${bpVars.user.face})`);
-
-
-        // TODO 加搜索焦点效果、清空按钮
-        const $search = $('#header-search');
-        const $search_input = $search.find('input');
-        $search.keyup((e) => {
-            const search_text = $search_input.val();
-            if (e.which === 13
-                && search_text.length > 0) {
-                utils.search(search_text);
-            }
-        });
-    });
 });

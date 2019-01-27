40289d6f53e14d7b8fbad223210a5bf4286cd3df
SettingDust <settingdust@gmail.com>
2019-01-28 04:36:26 +0800

    Add index.php

--- src/javascript/router.js
+++ src/javascript/router.js
@@ -10,6 +10,7 @@ export default async () => {
     bpLogger.debug(href);
     //Index
     router.push(new Route('$', import('./routes/index')));
+    router.push(new Route('index.php$', import('./routes/index')));
     //Video
     router.push(new Route('av\\d+', import('./routes/video/redirect')));
     // router.push(new Route('video/av\d+$', syncImport('./routes/video')));
